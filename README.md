dd3f6098d9bd57decf94d00bf5ef0ccfdfe17a8c
lkilcommons <liamk@nut.colorado.edu>
2015-03-24 14:27:15 -0600

    Update msispy.msis vectorized interface to allow specification of drivers manually as well as lookup by date

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ This model is maintained and developed by the Naval Research Laboratory, Space S
 
 **SuperDARN's contribution consists of Python wrappers to this model. This contribution is covered under DaViTpy license.**
 **The CU-SEDA contribution consists of making this model useable independent of DavitPy**
+
+We have added some additional functionality to SuperDARNs wrapper which vectorizes the call to the model
+and provides the return values in a dictionary with keys that name each variable. The interface is designed to 
+generally useful as well as compatible with [AtModWeb](https://github.com/lkilcommons/atmodweb) and [AtModExplorer](https://github.com/lkilcommons/atmodexplorer)
+
+The msispy.msis function is the vectorized interface. See it's source for more info.
--- msispy/__init__.py
+++ msispy/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2012  VT SuperDARN Lab
 # Full license can be found in LICENSE.txt
 # Modified by Liam Kilcommons, CU Boulder, 2014
+# (C) 2014 University of Colorado AES-CCAR-SEDA (Space Environment Data Analysis) Group
 """
 *********************
 **Module**: models.msis
@@ -94,6 +95,12 @@ Obtain F107 and AP required for MSIS input from tabulated values in IRI data.
 	for ldat in data:
 		yy = int(ldat[1:3])
 		year = 1900+yy if (yy >= 58) else 2000+yy
+		#Add a little extra flow control
+		if mydatetime is not None:
+			if year < mydatetime.year:
+				continue
+			elif year > mydatetime.year:
+				break
 		tdate.append( datetime(year, int(ldat[4:6]), int(ldat[7:9])).date() )
 		ttap = []
 		for iap in xrange(8):
@@ -140,13 +147,16 @@ Obtain F107 and AP required for MSIS input from tabulated values in IRI data.
 
 	return dictOut
 
-def msis(dt,lat,lon,alt):
+def msis(lat,lon,alt,dt=None,f107=None,ap_daily=None,f107a=None,ap3=None,ap33=None,ap36=None,ap39=None,apa1233=None,apa3657=None):
 	import numpy
 	from collections import OrderedDict
 	import datetime
 	"""
-	Calls the MSIS model for a single time and
-	an array of GEODETIC latitudes and longitudes and altitudes
+	Calls the MSIS model with given solarwind inputs or with those of datetime dt if specified
+	if the extra average and historical inputs are not specified,
+	then will just use the values of f107 and ap to replace all averages
+	
+	Pass arrays of GEODETIC latitudes and longitudes and altitudes. 
 
 		INPUTS
 		------
@@ -154,7 +164,26 @@ def msis(dt,lat,lon,alt):
 			lat - a [m x 1] numpy array of latitudes
 			lon - a [m x 1] numpy array of longitudes
 			alt - a [m x 1] numpy array of altitudes
-
+			f107 - float
+				daily f10.7 flux for previous day
+			ap_daily - float
+				daily AP magnetic index
+			f107a - optional,float
+				81 day average of f10.7 flux (centered on date)
+			ap3 - optional, float
+				3 hour AP for current time
+			ap33 - optional, float
+				3 hour AP for current time - 3 hours
+			ap36 - optional, float
+				3 hour AP for current time - 6 hours
+			ap39 - optional, float
+				3 hour AP for current time - 9 hours
+			apa1233 - optional, float
+				Average of eight 3 hour AP indicies from 12 to 33 hrs prior to current time
+			apa3657 
+				Average of eight 3 hour AP indices from 36 to 57 hours prior to current time
+			
+	 
 		RETURNS
 		-------
 			species - a OrderedDictionary of [m x 1] numpy arrays
@@ -177,7 +206,7 @@ def msis(dt,lat,lon,alt):
 				(5) 3 HR AP index for 9 hours before current time
 				(6) Average of eight 3 hour AP indicies from 12 to 33 hrs prior to current time
 				(7) Average of eight 3 hour AP indicies from 36 to 57 hrs prior to current time   
-	"""     
+	"""			
 	# * :func:`msispy.msisFort.gtd7`
 		# * **INPUTS**:
 		#   * **IYD** - year and day as YYDDD (day of year from 1 to 365 (or 366)) (Year ignored in current model)
@@ -228,7 +257,55 @@ def msis(dt,lat,lon,alt):
 	# * (6) Average of eight 3 hour AP indicies from 12 to 33 hrs prior to current time
 	# * (7) Average of eight 3 hour AP indicies from 36 to 57 hrs prior to current time
 	# # Get the F10.7 and AP values for the current year
-	drivers = getF107Ap(dt)
+	#Sanity check inputs
+	reference_shape = lat.flatten().shape
+
+	if lon.flatten().shape != reference_shape or alt.flatten().shape != reference_shape:
+		raise RuntimeError('Latitude, Longitude and Altitude arrays must be same shape!')
+
+	#Handle manual vs. date lookup driver input
+	if dt is not None: #If user has given a date use that to look up drivers
+		realdrivers = getF107Ap(dt)
+		#Pull the nessecary values so we don't break the logic
+		if f107 is None:
+			f107 = realdrivers['f107']
+		if ap_daily is None:
+			ap_daily = realdrivers['ap'][0] #daily
+	else:
+		if f107 is None or ap is None:
+			raise RuntimeError('Please specify either (1) A datetime for driver lookup (dt=datetime.datetime), or (2) F10.7 and Daily AP values (f107=float,ap=float)')
+		# Warn user if they are using no date and no historical driver info
+		if f107a is None and ap3 is None and ap33 is None and ap36 is None and ap39 is None and apa1233 is None and apa3657 is None:
+			print("--Warning! By not entering a date, and not specifing any of the historical/average drivers\n")
+			print("--MSIS will be run with the following 'approximations'\n")
+			print("--81 day average F10.7 = User given F10.7 (no seasonality!)\n")
+			print("--3 HR AP history at t minus 3, 6, 9 12-33 hr average and 36-57 hr average set to user given daily AP!\n")
+			
+		# Create some fake 'realdrivers' by replicating the user's f10.7 and daily ap inputs into all of the average/historical value slots
+		# 
+		realdrivers = dict()
+		realdrivers['datetime']=None
+		realdrivers['f107']=f107
+		realdrivers['f107a']=f107
+		realdrivers['ap']=[ap for i in range(7)]
+
+	drivers = dict()
+	
+	#Create the model drivers based on what the user has filled out (and left blank)
+	#I know the ternery operators are a little confusing, but it's so much eaiser to read
+	drivers['f107'] = f107 if f107 is not None else realdrivers['f107']
+	drivers['f107a'] = f107a if f107a is not None else realdrivers['f107a']
+	drivers['ap_daily'] = ap_daily
+	drivers['ap']=realdrivers['ap']
+	drivers['dt']=dt
+	drivers['ap'][0] = ap_daily if ap_daily is not None else realdrivers['ap'][0]
+	drivers['ap'][1] = ap3 if ap3 is not None else realdrivers['ap'][1]
+	drivers['ap'][2] = ap33 if ap33 is not None else realdrivers['ap'][2]
+	drivers['ap'][3] = ap36 if ap36 is not None else realdrivers['ap'][3]
+	drivers['ap'][4] = ap39 if ap39 is not None else realdrivers['ap'][4]
+	drivers['ap'][5] = ap1233 if apa1233 is not None else realdrivers['ap'][5]
+	drivers['ap'][6] = ap3657 if apa3657 is not None else realdrivers['ap'][6]
+	
 	npts = len(lat.flatten())
 	temp = numpy.empty((npts,1))
 	temp.fill(numpy.nan)
@@ -250,7 +327,10 @@ def msis(dt,lat,lon,alt):
 					slt,drivers['f107'],drivers['f107a'],drivers['ap'],48)
 		for i,atom in enumerate(species):
 			species[atom][p,0] = d[i]
+			if d[i] < 0.:
+				print "Warning: negative values output by msis!"
 		t_exo[p] = t[0]
 		t_alt[p] = t[1]
 
 	return species,t_exo,t_alt,drivers
+
